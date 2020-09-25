8b8a89a0f1796cbae6f7de52684ef2d435510ab1
navya <S538339@nwmissouri.edu>
2020-09-24 21:47:05 -0500

    Completed HelloWorld program

---
+++ Demo project/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by student on 9/23/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Started working on the project
+        print("temp")
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
